592ce0ca8b8719c660b0ebace3d8d40f58bdd8ab
mpetty <michael.petty@gmail.com>
2019-02-01 17:59:21 -0800

    moved logic to functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,15 +3,20 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-let cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
+function checkForMatch() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+      } else {
+        alert("Sorry, try again.");
+      }
+    }
   }
+function flipCard(cardId) {
+  console.log("User flipped: " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
 }
+
+flipCard(0);
+flipCard(2);
